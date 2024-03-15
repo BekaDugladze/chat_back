ce79e55bbe70a3e8a6e4a5610cb3fefb2e11af03
Beka Dugladze <125121125+BekaDugladze@users.noreply.github.com>
2024-03-15 12:44:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -40,13 +40,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: 'https://chat-pied-sigma.vercel.app',
-        methods: 'GET, POST, PUT, DELETE',
-        credentials: true,
-    })
-)
 
 app.get('/', (req, res, next) => {
     res.send('<a href="/auth/google">google</a>');
